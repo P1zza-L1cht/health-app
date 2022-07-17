2751e1549b1ce6d1e63e1015caa7e597875e3062
P1zza-L1cht <haruki21corder@gmail.com>
2022-07-17 16:37:42 +0900

    packages recoil, modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css';
 import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 export default function MyApp({
   Component,
@@ -7,9 +11,11 @@ export default function MyApp({
 }) {
   return(
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
-  )
+  );
 }
 
 
